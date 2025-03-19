9e9110251f3f7eefd3f4b5ca03b24bc20f9b205c
Grace Hanson <hansongrace2004@gmail.com>
2025-03-18 21:30:10 -0400

    NodeGUI
    Add NodeGUI for javascript GUI. Program planned to be built in purely Javascript.

--- app.js
+++ app.js
@@ -1,14 +1,1 @@
-const express = require('express')
-
-
-app = express()
-
-app.get('/', (req, res) => {
-res.send('hello world');
-
-});
-//create localhost to run js file
-app.listen(3000, () =>{
-    //callback function
-console.log('listening')
-});
+const { app, Window, Text } = require('@nodegui/nodegui');

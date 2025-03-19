4ddc47ec42d803eabb1822161016a11c8079d5a5
Grace Hanson <hansongrace2004@gmail.com>
2025-03-18 21:08:43 -0400

    Rework files

--- imageProcessor.js
+++
@@ -1,9 +0,0 @@
-const sharp = require("sharp");
-
-async function resizeImage(inputPath, outputPath, width, height){
-    await sharp(inputPath)
-        .resize(width, height, { fit: "cover"})
-        .toFile(outputPath);
-
-}
-module.exports = { resizeImage };
